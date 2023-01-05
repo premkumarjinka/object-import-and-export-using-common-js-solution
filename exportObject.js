5e0cfeac3bdddd8d3ee25cf67fdc1c688e64cc92
premkumar <premkumar.jinka@gmail.com>
2023-01-05 16:39:18 +0000

    object export and import solution using common js module

---
+++ exportObject.js
@@ -0,0 +1,9 @@
+class Object {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+const val = new Object("John", "Wilson");
+
+module.exports = val;
